0ab1366e69bfa645c5bdff06710fef8821f8a779
Vladimir Osintsev <osintsev@gmail.com>
2010-02-25 01:13:51 +0300

    Initial package debianization

---
+++ debian/glb.default.ex
@@ -0,0 +1,10 @@
+# Defaults for glb initscript
+# sourced by /etc/init.d/glb
+# installed at /etc/default/glb by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
